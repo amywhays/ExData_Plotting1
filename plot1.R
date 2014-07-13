b27fa7503ba7d9b6e76ba8eb2953ae02ff74256f
Amy Hays <amywhays@gmail.com>
2014-07-13 00:03:14 -0500

    Added some notes to Plot1.R

--- plot1.R
+++ plot1.R
@@ -1,10 +1,14 @@
 
 
-
+## Loading data for Feb. 1-2, 2007, into a data frame 
 
 md <- read.table(pipe('grep "^[1-2]/2/2007" "household_power_consumption.txt"'),header=F, sep=';', stringsAsFactors=FALSE)
+
+## Adding the column names. 
+
 colnames(md) <-names(read.table('household_power_consumption.txt', header=TRUE,sep=";",nrows=1))
 
+## Combining the date and time fields to make it easier to convert the field class.
 dt <- paste(md$Date, md$Time, sep=" ")
 
 
@@ -13,7 +17,6 @@ datetime <- strptime(dt, format="%d/%m/%Y %H:%M:%S")
 data <- cbind(datetime, md)
 
 png(filename="./ExData_Plotting1/plot1.png", width = 480, height = 480)
+
 hist(data$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power")
 dev.off()
-
-
